fa36f2b4fc3d6682be1d223dafa2f21ca52352b7
carreymu <muwei1984@163.com>
2020-04-12 22:25:32 +0800

    add news for targetIndex

---
+++ src/utils/utdate.js
@@ -0,0 +1,40 @@
+var date_ops= {
+    date_fmt: function(input_date, fmt){
+        let d = new Date()
+        if(input_date instanceof Date){
+            d = input_date
+        } else {
+            // timestampe
+            let td = new Date(input_date)
+            if(td == 'Invalid Date'){
+                return input_date
+            } else {
+                if(td.getFullYear() == 1970){
+                    return input_date
+                } else{
+                    d = td
+                }
+            }
+        }
+        var o = {
+            "M+": d.getMonth() + 1,                 //月份 
+            "d+": d.getDate(),                    //日 
+            "h+": d.getHours(),                   //小时 
+            "m+": d.getMinutes(),                 //分 
+            "s+": d.getSeconds(),                 //秒 
+            "q+": Math.floor((d.getMonth() + 3) / 3), //季度 
+            "S": d.getMilliseconds()             //毫秒 
+        };
+        if (/(y+)/.test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (d.getFullYear() + "").substr(4 - RegExp.$1.length));
+        }
+        for (var k in o) {
+            if (new RegExp("(" + k + ")").test(fmt)) {
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+            }
+        }
+        return fmt;
+    }
+
+}
+export default date_ops
