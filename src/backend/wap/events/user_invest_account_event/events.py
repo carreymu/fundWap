e694df8647c820152fc94769cf4754e66a0dfa51
carreymu <muwei1984@163.com>
2020-06-20 22:52:06 +0800

    change resource

--- src/backend/wap/events/user_invest_account_event/events.py
+++ src/backend/wap/events/user_invest_account_event/events.py
@@ -4,6 +4,7 @@ from wap.base import DataSource
 from wap.data_source import exec_base
 import time
 
+
 class UserInvestAccountJoined(DataSource):
     # req: dict
     event_default: Any
@@ -14,6 +15,7 @@ class UserInvestAccountJoined(DataSource):
         if user_iv_acc_list:
             user_invest = {}
             my_info = {}
+            dt = time.strftime('%m月%d日', time.localtime(time.time()))
             user_iv_acc_list = user_iv_acc_list['user_invest_account_by_uid']
             all_hold_profit = sum([x['hold_profit'] for x in user_iv_acc_list])
             init_amt = sum([x['init_amt'] for x in user_iv_acc_list])
@@ -21,14 +23,23 @@ class UserInvestAccountJoined(DataSource):
             my_info['all_profit'] = round(all_hold_profit, 2)
             my_info["hold_profit"] = round(sum([x['hold_profit'] for x in user_iv_acc_list if x['hold_status'] != 0]), 2)
             my_info["daily_profit"] = round(sum([x['daily_profit'] for x in user_iv_acc_list]), 2)
+            my_info["now"] = dt
 
             my_invests = []
-            if user_iv_acc_list:
-                dt = time.strftime('%m-%d', time.localtime(time.time()))
-                my_invests.append(self.json_convert(user_iv_acc_list, 'tid', '大目标', dt))
-                my_invests.append(self.json_convert(user_iv_acc_list, 'fpl_id', '优选', dt))
-                my_invests.append(self.json_convert(user_iv_acc_list, 'did', '鸡腿计划', dt))
-                my_invests.append(self.json_convert(user_iv_acc_list, 'fid', '基金', dt))
+            user_iv_acc = [x for x in user_iv_acc_list if x['hold_status'] != 0]
+            if user_iv_acc:
+                targets = self.json_convert(user_iv_acc, 'tid', '大目标', dt)
+                if targets:
+                    my_invests.append(targets)
+                best_choices = self.json_convert(user_iv_acc, 'fpl_id', '优选', dt)
+                if best_choices:
+                    my_invests.append(best_choices)
+                drumbs = self.json_convert(user_iv_acc, 'did', '鸡腿计划', dt)
+                if drumbs:
+                    my_invests.append(drumbs)
+                funds = self.json_convert(user_iv_acc, 'fid', '基金', dt)
+                if funds:
+                    my_invests.append(funds)
             user_invest["my_invests"] = my_invests
 
             # tid->ft_id->fid 有9笔赎回记录即将到帐,最早预计05-07到帐
@@ -36,18 +47,18 @@ class UserInvestAccountJoined(DataSource):
             tars_list = await exec_base.exec_sql_key(event_names='targets_by_tids', **{'tids': tids})
             fids = ','.join(['%s' % x['ft_id'] for x in tars_list])
 
-            uia_ids = ','.join(['%s' % x['uia_id'] for x in user_iv_acc_list if x['type'] == 'tid'])
+            uia_ids = '\',\''.join(['%s' % x['uia_id'] for x in user_iv_acc_list if x['type'] == 'tid'])
             uid = user_iv_acc_list[0]['uid']
-            import pdb;pdb.set_trace()
-            user_iv_acc_detail_list = await exec_base.exec_sql_key(event_names='user_invest_account_details_by_ids',
-                                                                   **{'uid': uid, 'uia_ids': uia_ids, 'fids': fids})
-            if user_iv_acc_detail_list:
-                # hold_status 0-赎回到帐(已清仓),1-持仓,2-赎回中
-                hd_user_iv_acc = [x for x in user_iv_acc_detail_list if x['hold_status'] == 2]
-                if hd_user_iv_acc:
-                    ft_hd_user_iv_acc = sorted(hd_user_iv_acc, key=lambda x: x['pay_date'], reverse=False)
-                    my_info['pay_date'] = ft_hd_user_iv_acc[0]['pay_date']
-                    my_info['redeem_cnt'] = len(ft_hd_user_iv_acc)
+            # import pdb;pdb.set_trace()
+            # user_iv_acc_detail_list = await exec_base.exec_sql_key(event_names='user_invest_account_details_by_ids',
+            #                                                        **{'uid': uid, 'uia_ids': uia_ids, 'fids': fids})
+            # if user_iv_acc_detail_list:
+            #     # hold_status 0-赎回到帐(已清仓),1-持仓,2-赎回中
+            #     hd_user_iv_acc = [x for x in user_iv_acc_detail_list if x['hold_status'] == 2]
+            #     if hd_user_iv_acc:
+            #         ft_hd_user_iv_acc = sorted(hd_user_iv_acc, key=lambda x: x['pay_date'], reverse=False)
+            #         my_info['pay_date'] = ft_hd_user_iv_acc[0]['pay_date']
+            #         my_info['redeem_cnt'] = len(ft_hd_user_iv_acc)
             user_invest["my_info"] = my_info
             return user_invest
         return self.event_default
--- src/components/wap/mine/js/myPositions.js
+++ src/components/wap/mine/js/myPositions.js
@@ -22,73 +22,81 @@ export default {
     myPositions(){
       let dt = {
         "req": {"uid":1},
-        "event_names": ["user_invest_account_by_uid"]
+        "event_names": ["user_invest_account_joined_by_uid"]
       }
       this.$api.fetchPost('/sanic-api', dt).then(r=>{
-        if(r.user_invest_account_by_uid!=undefined && r.user_invest_account_by_uid.length>0){
-          let lst = r.user_invest_account_by_uid
-          let init_amt = eval(lst.map(x=>x.init_amt).join("+"))
-          let all_hold_profit = eval(lst.map(x=>x.hold_profit).join("+"))
-          this.myInfo["all_amt"] = (init_amt+all_hold_profit).toFixed(2)
-          // 赎回+持有,部分赎回也属赎回
-          this.myInfo["all_profit"] = all_hold_profit.toFixed(2)
-          this.myInfo["now"] = this.$utdate.dateFmt(new Date(),"MM月dd日")
-          // 持有+赎回中
-          let hold_profit = eval(lst.filter(x=>x.hold_status!=0).map(x=>x.hold_profit).join("+"))
-          this.myInfo["hold_profit"] = hold_profit.toFixed(2)
-          this.myInfo["daily_profit"] = eval(lst.map(x=>x.daily_profit).join("+")).toFixed(2)
+        if(r.user_invest_account_joined_by_uid!=undefined){
+          let res = r.user_invest_account_joined_by_uid
+          this.myInfo=res['my_info']
 
-          // hold_profit = hold_profit_yesterday+daily_profit
-          let hold_list = lst.filter(x=>x.hold_status!=0)
-          if(hold_list.length==0) return
+          this.myInvest = res['my_invests']
+
+
+
+
+          // let lst = r.user_invest_account_by_uid
+          // let init_amt = eval(lst.map(x=>x.init_amt).join("+"))
+          // let all_hold_profit = eval(lst.map(x=>x.hold_profit).join("+"))
+          // this.myInfo["all_amt"] = (init_amt+all_hold_profit).toFixed(2)
+          // // 赎回+持有,部分赎回也属赎回
+          // this.myInfo["all_profit"] = all_hold_profit.toFixed(2)
+          // this.myInfo["now"] = this.$utdate.dateFmt(new Date(),"MM月dd日")
+          // // 持有+赎回中
+          // let hold_profit = eval(lst.filter(x=>x.hold_status!=0).map(x=>x.hold_profit).join("+"))
+          // this.myInfo["hold_profit"] = hold_profit.toFixed(2)
+          // this.myInfo["daily_profit"] = eval(lst.map(x=>x.daily_profit).join("+")).toFixed(2)
+
+          // // hold_profit = hold_profit_yesterday+daily_profit
+          // let hold_list = lst.filter(x=>x.hold_status!=0)
+          // if(hold_list.length==0) return
           
-          this.myTargets = hold_list.filter(x=>x.type=="tid")
-          if(this.myTargets.length>0){
-            let hold_profit = eval(this.myTargets.map(x=>x.hold_profit).join("+"))
-            let day_profit = eval(this.myTargets.map(x=>x.daily_profit).join("+"))
-            let init_amt = eval(this.myTargets.map(x=>x.init_amt).join("+"))
-            let t_amt = init_amt+hold_profit
-            let td_ratio = (day_profit/t_amt).toFixed(2)
-            let t_ratio = ((hold_profit+day_profit)/t_amt).toFixed(2)
-            this.myInvest.push({"name":"大目标","dt": this.myInfo["now"], "daily_ratio":td_ratio,"daily_profit":day_profit.toFixed(2),
-            "hold_ratio":t_ratio,"hold_profit":hold_profit.toFixed(2),"hold_cnt":this.myTargets.length})
-          }
+          // this.myTargets = hold_list.filter(x=>x.type=="tid")
+          // if(this.myTargets.length>0){
+          //   let hold_profit = eval(this.myTargets.map(x=>x.hold_profit).join("+"))
+          //   let day_profit = eval(this.myTargets.map(x=>x.daily_profit).join("+"))
+          //   let init_amt = eval(this.myTargets.map(x=>x.init_amt).join("+"))
+          //   let t_amt = init_amt+hold_profit
+          //   let td_ratio = (day_profit/t_amt).toFixed(2)
+          //   let t_ratio = ((hold_profit+day_profit)/t_amt).toFixed(2)
+          //   this.myInvest.push({"name":"大目标","dt": this.myInfo["now"], "daily_ratio":td_ratio,"daily_profit":day_profit.toFixed(2),
+          //   "hold_ratio":t_ratio,"hold_profit":hold_profit.toFixed(2),"hold_cnt":this.myTargets.length})
+          // }
 
-          this.myChoice = hold_list.filter(x=>x.type=="fpl_id") //&& x.is_sched==0
-          if(this.myChoice.length>0){
-            let hold_profit = eval(this.myChoice.map(x=>x.hold_profit).join("+"))
-            let day_profit = eval(this.myChoice.map(x=>x.daily_profit).join("+"))
-            let init_amt = eval(this.myChoice.map(x=>x.init_amt).join("+"))
-            let t_amt = init_amt+hold_profit
-            let td_ratio = (day_profit/t_amt).toFixed(2)
-            let t_ratio = ((hold_profit+day_profit)/t_amt).toFixed(2)
-            this.myInvest.push({"name":"优选","dt": this.myInfo["now"], "daily_ratio":td_ratio,"daily_profit":day_profit.toFixed(2),
-            "hold_ratio":t_ratio,"hold_profit":hold_profit.toFixed(2),"hold_cnt":0})
-          }
+          // this.myChoice = hold_list.filter(x=>x.type=="fpl_id") //&& x.is_sched==0
+          // if(this.myChoice.length>0){
+          //   let hold_profit = eval(this.myChoice.map(x=>x.hold_profit).join("+"))
+          //   let day_profit = eval(this.myChoice.map(x=>x.daily_profit).join("+"))
+          //   let init_amt = eval(this.myChoice.map(x=>x.init_amt).join("+"))
+          //   let t_amt = init_amt+hold_profit
+          //   let td_ratio = (day_profit/t_amt).toFixed(2)
+          //   let t_ratio = ((hold_profit+day_profit)/t_amt).toFixed(2)
+          //   this.myInvest.push({"name":"优选","dt": this.myInfo["now"], "daily_ratio":td_ratio,"daily_profit":day_profit.toFixed(2),
+          //   "hold_ratio":t_ratio,"hold_profit":hold_profit.toFixed(2),"hold_cnt":0})
+          // }
 
-          this.myDrumstick = hold_list.filter(x=>x.type=="did")
-          if(this.myDrumstick.length>0){
-            let hold_profit = eval(this.myDrumstick.map(x=>x.hold_profit).join("+"))
-            let day_profit = eval(this.myDrumstick.map(x=>x.daily_profit).join("+"))
-            let init_amt = eval(this.myDrumstick.map(x=>x.init_amt).join("+"))
-            let t_amt = init_amt+hold_profit
-            let td_ratio = (day_profit/t_amt).toFixed(2)
-            let t_ratio = ((hold_profit+day_profit)/t_amt).toFixed(2)
-            this.myInvest.push({"name":"鸡腿计划","dt": this.myInfo["now"], "daily_ratio":td_ratio,"daily_profit":day_profit.toFixed(2),
-            "hold_ratio":t_ratio,"hold_profit":hold_profit.toFixed(2),"hold_cnt":0})
-          }
+          // this.myDrumstick = hold_list.filter(x=>x.type=="did")
+          // if(this.myDrumstick.length>0){
+          //   let hold_profit = eval(this.myDrumstick.map(x=>x.hold_profit).join("+"))
+          //   let day_profit = eval(this.myDrumstick.map(x=>x.daily_profit).join("+"))
+          //   let init_amt = eval(this.myDrumstick.map(x=>x.init_amt).join("+"))
+          //   let t_amt = init_amt+hold_profit
+          //   let td_ratio = (day_profit/t_amt).toFixed(2)
+          //   let t_ratio = ((hold_profit+day_profit)/t_amt).toFixed(2)
+          //   this.myInvest.push({"name":"鸡腿计划","dt": this.myInfo["now"], "daily_ratio":td_ratio,"daily_profit":day_profit.toFixed(2),
+          //   "hold_ratio":t_ratio,"hold_profit":hold_profit.toFixed(2),"hold_cnt":0})
+          // }
 
-          this.myfunds = hold_list.filter(x=>x.type=="fid")
-          if(this.myfunds.length>0){
-            let hold_profit = eval(this.myfunds.map(x=>x.hold_profit).join("+"))
-            let day_profit = eval(this.myfunds.map(x=>x.daily_profit).join("+"))
-            let init_amt = eval(this.myfunds.map(x=>x.init_amt).join("+"))
-            let t_amt = init_amt+hold_profit
-            let td_ratio = (day_profit/t_amt).toFixed(2)
-            let t_ratio = ((hold_profit+day_profit)/t_amt).toFixed(2)
-            this.myInvest.push({"name":"基金","dt": this.myInfo["now"], "daily_ratio":td_ratio,"daily_profit":day_profit.toFixed(2),
-            "hold_ratio":t_ratio,"hold_profit":hold_profit.toFixed(2),"hold_cnt":0})
-          }
+          // this.myfunds = hold_list.filter(x=>x.type=="fid")
+          // if(this.myfunds.length>0){
+          //   let hold_profit = eval(this.myfunds.map(x=>x.hold_profit).join("+"))
+          //   let day_profit = eval(this.myfunds.map(x=>x.daily_profit).join("+"))
+          //   let init_amt = eval(this.myfunds.map(x=>x.init_amt).join("+"))
+          //   let t_amt = init_amt+hold_profit
+          //   let td_ratio = (day_profit/t_amt).toFixed(2)
+          //   let t_ratio = ((hold_profit+day_profit)/t_amt).toFixed(2)
+          //   this.myInvest.push({"name":"基金","dt": this.myInfo["now"], "daily_ratio":td_ratio,"daily_profit":day_profit.toFixed(2),
+          //   "hold_ratio":t_ratio,"hold_profit":hold_profit.toFixed(2),"hold_cnt":0})
+          // }
         }
       })
     },
