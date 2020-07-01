c1e3775a225bea18f49f5bc942637996ba6aa952
carreymu <muwei1984@163.com>
2020-07-01 22:04:01 +0800

    fund_templates:percentage to hold_percentage

--- src/backend/wap/utils/sql_handler.py
+++ src/backend/wap/utils/sql_handler.py
@@ -1,5 +1,9 @@
 
 
 def sql_in(lst: list):
-    fmt = "%s" if isinstance(lst[0], int) else "'%s'"
-    return ",".join(fmt % str(x) for x in lst)
+    if isinstance(lst[0], int):
+        return f"{lst[0]}" if len(lst) == 1 else ",".join("%s" % str(x) for x in lst)
+    else:
+        return f"'{lst[0]}'" if len(lst) == 1 else ",".join("'%s'" % str(x) for x in lst)
+    # fmt = "%s" if isinstance(lst[0], int) else "'%s'"
+    # return ",".join(fmt % str(x) for x in lst)
